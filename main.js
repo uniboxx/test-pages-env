dc3e9247f202f0ab561c32362d8799965f5fa7f5
uniboxx <unibox@duck.com>
2024-10-12 20:53:01 +0200

    move meta

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 import './style.css';
 
-const word = import.meta.env.VITE_WORD;
-const prova = import.meta.env.VITE_PROVA;
 console.log(word);
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <p>${word}</p>
-    <p>${prova}</p>
+    <p>${import.meta.env.VITE_WORD}</p>
   </div>
 `;
