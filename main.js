27c3ec5e9c0935baf7a6582f4fa2f1263bdaf7e6
uniboxx <unibox@duck.com>
2024-10-12 20:24:13 +0200

    add prova

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import './style.css';
 
 const word = import.meta.env.VITE_WORD;
+const prova = import.meta.env.VITE_PROVA;
 console.log(word);
 
 document.querySelector('#app').innerHTML = `
   <div>
     <p>${word}</p>
+    <p>${prova}</p>
   </div>
 `;
